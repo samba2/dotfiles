cc99cafed828bdd55a6c247ce1744b602b330532
samba2 <samba2@users.noreply.github.com>
2016-04-17 22:21:28 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,4 @@
-# possible locations
-# "$Home\Documents": put the file here to execute it only for your user.
-# $PsHome: put the file here to execute it for every user.
+# possible locations:
+# did not work on win 10: "$Home\Documents": put the file here to execute it only for your user. 
+# worked in win 10: $PsHome: put the file here to execute it for every user.
 new-item -path alias:find -value Get-ChildItem

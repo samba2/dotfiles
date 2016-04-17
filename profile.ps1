f39eba48a80f92bb4df0251e4e9b381972b348b6
samba2 <samba2@users.noreply.github.com>
2016-04-17 22:10:58 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,2 +1,4 @@
-# overwrite find cmd command
+# possible locations
+# "$Home\Documents": put the file here to execute it only for your user.
+# $PsHome: put the file here to execute it for every user.
 new-item -path alias:find -value Get-ChildItem
